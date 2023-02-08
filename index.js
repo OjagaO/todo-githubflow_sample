53738a1274ec2f26e47f80b559afbce804af1a57
OjagaO <ahiroto1234@gmail.com>
2023-02-08 14:55:39 +0900

    完了未完了の線を追加しました

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 $("#btn").click(function () { 
-    console.log("----------------");
     let val = $("#input").val();
-    // let li = document.createElement("li");
-    // let todo = .text(val);
-    $("#todos").append("<li><input type = 'checkbox'>"+ val + "</li>");
-    $("#input").val("");
+    if(val.length > 0){
+        $("#todos").append("<li><input type = 'checkbox'>"+ val + "</li>");
+        $("#input").val("");
+    }
 });
+
+$(document).on("change","input[type = checkbox]",function(){
+    if($(this).is(":checked")){
+        $(this).parent().css("text-decoration","line-through")
+    } else if(!$(this).is("checked")){
+        $(this).parent().css("text-decoration","none")
+    }
+})
